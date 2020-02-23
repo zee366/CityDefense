ae365c1ae4dd8bea2c1e0975600c4032a1adb824
VincentBerardi <vincent.l.berardi@gmail.com>
2020-02-23 16:19:04 -0500

    Adding Wave and Wave Manager Not currently functional!

--- Assets/Scripts/AIAgent.cs
+++ Assets/Scripts/AIAgent.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AIAgent : MonoBehaviour
 {
     public float aggressionLevel;
+    //dictionary/list of action state variable
 
     // Start is called before the first frame update
     void Start()
