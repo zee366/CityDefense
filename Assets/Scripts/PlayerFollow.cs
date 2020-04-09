30ad7f187a82224faf5fad327e78e3cb9a5ab7e0
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-04-09 15:38:20 -0400

    NavMeshComponents and PlayerFollow Camera Component.

---
+++ Assets/Scripts/PlayerFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+
+    public Transform PlayerTransform;
+
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+
+    public bool LookAtPlayer = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    // LateUpdate is called after Update methods
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+
+        if (LookAtPlayer)
+            transform.LookAt(PlayerTransform);
+    }
+}
