cde60232782da54aa599f5b1e63baa1be6878564
VincentBerardi <vincent.l.berardi@gmail.com>
2020-04-10 13:11:36 -0400

    Setup for PoliceAbilities

---
+++ Assets/Scripts/UI/PoliceAbilities.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceAbilities : MonoBehaviour
+{
+
+    public void OnRubberBulletsButtonClicked()
+    {
+        //Logic of rubber bullets selected for police
+    }
+    public void OnSmokeGrendadeButtonClicked()
+    {
+        //Logic of smoke grenade selected for police
+    }
+    public void OnWaterCannonButtonClicked()
+    {
+        //Logic of water cannon selected for police
+    }
+    public void OnLethalForceButtonClicked()
+    {
+        //Logic of lethal force selected for police
+    }
+}
