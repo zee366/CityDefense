372960c98c8f653aa15a725bbf1dc6057f2520c1
Michael Mekies <43508872+mmekies@users.noreply.github.com>
2020-04-11 12:24:47 -0400

    Flock

---
+++ Assets/Flock/flocking-algorithm-master/FlockBehavior.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class FlockBehavior : ScriptableObject
+{
+    public abstract Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock);
+}
