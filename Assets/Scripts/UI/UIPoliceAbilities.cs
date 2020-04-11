69814ca7ba44cc0ab3849120d8b9cf25539d718c
VincentBerardi <vincent.l.berardi@gmail.com>
2020-04-11 17:36:02 -0400

    -Added cooldowns to some ability buttons -Added switching between bullet types -Added logic to grenade throwing in Police Abilities -Added grenade script logic with area of effect for damage -Need to add animation of smoke -Missing bullet firing, water cannon, and reinforce logic
    -Refacoring likely based on squads logic

---
+++ Assets/Scripts/UI/UIPoliceAbilities.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPoliceAbilities : MonoBehaviour
+{
+    public PoliceAbilities policeSquadLeader;
+
+    
+    float smokeGTimer;
+    float smokeGTimerDelay;
+    bool smokeGrenadeCoolDown;
+
+    float waterCTimer;
+    float waterCTimerDelay;
+    bool waterCannonCoolDown;
+
+    float reinforceTimer;
+    float reinforceTimerDelay;
+    bool reinforceCoolDown;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smokeGTimer = 2.0f;
+        smokeGTimerDelay = 2.0f;
+        smokeGrenadeCoolDown = false;
+
+        waterCTimer = 3.0f;
+        waterCTimerDelay = 3.0f;
+        waterCannonCoolDown = false;
+
+        reinforceTimer = 2.0f;
+        reinforceTimerDelay = 2.0f;
+        reinforceCoolDown = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (smokeGrenadeCoolDown)
+        {
+            smokeGTimer -= Time.deltaTime;
+            if (smokeGTimer <= 0)
+            {
+                smokeGTimer = smokeGTimerDelay;
+                smokeGrenadeCoolDown = false;
+            }
+        }
+
+        if (waterCannonCoolDown)
+        {
+            waterCTimer -= Time.deltaTime;
+            if (waterCTimer <= 0)
+            {
+                waterCTimer = waterCTimerDelay;
+                waterCannonCoolDown = false;
+            }
+        }
+
+        if (reinforceCoolDown)
+        {
+            reinforceTimer -= Time.deltaTime;
+            if (reinforceTimer <= 0)
+            {
+                reinforceTimer = reinforceTimerDelay;
+                reinforceCoolDown = false;
+            }
+        }
+    }
+
+
+    public void OnArrestButtonClicked()
+    {
+        //Logic of arrest selected for police
+        policeSquadLeader.Arrest();
+    }
+    
+    public void OnAggressiveArrestButtonClicked()
+    {
+        //Logic of aggressive arrest selected for police
+        policeSquadLeader.AggressiveArrest();
+    }
+
+    public void OnRubberBulletsButtonClicked()
+    {
+        //Logic of rubber bullets selected for police
+        policeSquadLeader.UseRubberBullets();
+    }
+    public void OnSmokeGrendadeButtonClicked()
+    {
+        //Logic of smoke grenade selected for police
+        if (!smokeGrenadeCoolDown)
+        {
+            policeSquadLeader.UseSmokeGrenade();
+            smokeGrenadeCoolDown = true;
+        }
+    }
+    public void OnWaterCannonButtonClicked()
+    {
+        //Logic of water cannon selected for police
+        if (!waterCannonCoolDown)
+        {
+            policeSquadLeader.UseWaterCannon();
+            waterCannonCoolDown = true;
+        }
+    }
+    public void OnLethalForceButtonClicked()
+    {
+        //Logic of lethal force selected for police
+        policeSquadLeader.UseLethalBullets();
+    }
+    
+    public void OnReinforceSquadButtonClicked()
+    {
+        //Logic of reinforce selected for police
+        if (!reinforceCoolDown)
+        {
+            policeSquadLeader.ReinforceSquad();
+            reinforceCoolDown = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/PoliceAbilities.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PoliceAbilities : MonoBehaviour
+{
+    //change to actual class of it? even needed? 
+    //GameObject policeSquadLeader;
+
+    //rioters
+    //specify class
+    public TesterRioter rioterTarget;
+
+    //Grenade prefab
+    public GameObject smokeGrenade;
+    public float launchForce;
+
+    //bullets
+    //specify class?
+    public List<GameObject> bulletTypes;
+    GameObject bulletSetType;
+
+    //water cannon
+    GameObject waterCannon;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    public void Arrest()
+    {
+        //logic for less points deducted from PR
+
+        //have an animation for the arrest?
+        Destroy(rioterTarget);
+    }
+    
+    public void AggressiveArrest()
+    {
+        //logic for more points deducted from PR
+
+        //have an animation for the arrest?
+        Destroy(rioterTarget);
+    }
+
+    private void FireBullets()
+    {
+
+    }
+
+    public void UseRubberBullets()
+    {
+        bulletSetType = bulletTypes[0];
+    }
+
+    public void UseSmokeGrenade()
+    {
+        //logic for throwing grenade at an arch
+        GameObject sG = Instantiate(smokeGrenade, transform.forward + new Vector3(0, 1.5f, 0), Quaternion.identity);
+        sG.GetComponent<Rigidbody>().AddForce(transform.forward * launchForce, ForceMode.Impulse);
+    }
+
+    public void UseWaterCannon()
+    {
+        //logic for using water cannon
+    }
+
+    public void UseLethalBullets()
+    {
+        bulletSetType = bulletTypes[1];
+    }
+
+    public void ReinforceSquad()
+    {
+        //logic for summoning a police squad member
+
+    }
+}
