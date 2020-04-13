34957ab836608d48a190aeea4da19007ae8f5c8e
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-04-13 10:17:47 -0400

    Optimization: Minimap Camera Renders Only Once at Start
    - This doubles the frame rate, but the minimap needs to be manually updated to show any changes to the map. (TODO for map indicators).
    - Resolved small conflict with AgentPrefab.meta

---
+++ Assets/Minimap/MinimapController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapController : MonoBehaviour
+{
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make sure the camera is disabled in the Minimap Component prefab! Else performance will be very poor! 
+        cam.Render();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
