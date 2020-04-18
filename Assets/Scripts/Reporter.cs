dc3f993583def8388254cd200812f51cfb582b86
VincentBerardi <vincent.l.berardi@gmail.com>
2020-04-18 14:08:41 -0400

    -Added logic to the Reporter script -Still need to do some testing -Something other than the sphere collider is preventing policeflock from going in Reporter radius

--- Assets/Scripts/Reporter.cs
+++ Assets/Scripts/Reporter.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Reporter : MonoBehaviour
 {
+    private List<GameObject> listOfRiotersAndPolice;
+    [SerializeField]
+    private float mediaSceneRadius = 0;
+
     //will have a physics overlap logic
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        listOfRiotersAndPolice = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //listOfRiotersAndPolice = CheckIfNearPoliceAndRioters();
+    }
+
+    public List<GameObject> CheckIfNearPoliceAndRioters()
+    {
+        //Collider[] colliders = Physics.OverlapSphere(transform.position, mediaSceneRadius);
+
+        //foreach (Collider cd in colliders)
+        //{
+        //    if (cd.gameObject.GetComponent<PoliceAbilities>())
+        //        listOfRiotersAndPolice.Add(cd.gameObject);
+        //    else if (cd.gameObject.GetComponent<Rioters.RioterAgentBehavior>())
+        //        listOfRiotersAndPolice.Add(cd.gameObject);
+        //}
+
+        return listOfRiotersAndPolice;
     }
 }
