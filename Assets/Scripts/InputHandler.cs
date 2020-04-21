828df8d24aa88c21b80530bd87f5c1bfba3c208a
Jason Brennan <brenjaso@gmail.com>
2020-04-21 12:29:00 -0400

    Initial implementation of input handler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
@@ -10,13 +8,6 @@ public class InputHandler : MonoBehaviour
     private float lastMouseX;
     private float lastMouseY;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         CheckInput();
