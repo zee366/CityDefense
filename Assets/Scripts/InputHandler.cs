6ad1ba3311fc67f01aaaf89ca503536eeae11db1
Jason Brennan <brenjaso@gmail.com>
2020-04-21 11:31:18 -0400

    Beginning InputHandler behavior

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    public UIPoliceAbilities abilityBar;
+    public CameraController cameraController;
+
+    private float lastMouseX;
+    private float lastMouseY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        lastMouseX = Input.mousePosition.x;
+        lastMouseY = Input.mousePosition.y;
+    }
+
+    void CheckInput() {
+        if(Input.GetKeyDown(KeyCode.Alpha1)) {
+            abilityBar.OnArrestButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)) {
+            abilityBar.OnAggressiveArrestButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)) {
+            abilityBar.OnRubberBulletsButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)) {
+            abilityBar.OnFireBulletsButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5)) {
+            abilityBar.OnSmokeGrendadeButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha6)) {
+            abilityBar.OnWaterCannonButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha7)) {
+            abilityBar.OnLethalForceButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha8)) {
+            abilityBar.OnReinforceSquadButtonClicked();
+            return;
+        }
+        if(Input.GetKeyDown(KeyCode.C)) {
+            // FormCircle()
+        }
+        if(Input.GetKeyUp(KeyCode.C)) {
+            // for each flock agent?
+            // navMeshAgent.destination = transform.position
+        }
+        if(Input.GetKey(KeyCode.LeftShift)) {
+            if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+                // FormLineFacingLeft()
+                return;
+            }
+            if(Input.GetKeyDown(KeyCode.RightArrow)) {
+                // FormLineFacingRight()
+                return;
+            }
+            if(Input.GetKeyDown(KeyCode.UpArrow)) {
+                // FormLineFacingForward()
+                return;
+            }
+            if(Input.GetKeyDown(KeyCode.DownArrow)) {
+                // FormLineFacingBackward()
+                return;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)) {
+            // FaceLeft()
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)) {
+            // FaceRight()
+        }
+        if(Input.GetKeyDown(KeyCode.UpArrow)) {
+            // FaceForward()
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            // FaceDown()
+        }
+        if(Input.GetKey(KeyCode.Mouse2)) {
+            // cameraController.orbitfunction(mouse_dx, mouse_dy)
+        }
+    }
+}
