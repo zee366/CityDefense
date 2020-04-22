bab4a99658796d10a0132a61d0db7fce1815a7da
OmarAlFarajat <40572852+OmarAlFarajat@users.noreply.github.com>
2020-04-22 17:25:43 -0400

    Basic Surveillance Functionality Working
    - Checks if enough $ is accumulated
    - Changes what is visible on the minimap
    - Note: Riot spawns don't show

--- Assets/Scripts/Surveillance.cs
+++ Assets/Scripts/Surveillance.cs
@@ -6,34 +6,89 @@ using UnityEngine.UI;
 public class Surveillance : MonoBehaviour
 {
     public Image surveillanceImg;
-
+    public Button surveillanceButton;
     private PublicRelations publicRelations;
-    
+    private Camera miniMapCam;
     private enum SurveillanceLevel { Level_0, Level_1, Level_2, Level_3};
-    private int[] costs = new int[3] { 200,400,600};
-    private SurveillanceLevel current = SurveillanceLevel.Level_0; 
-
+    private Dictionary<SurveillanceLevel,int> costs;
+    private SurveillanceLevel current = SurveillanceLevel.Level_0;
+    public int Level1Cost = 200;
+    public int Level2Cost = 400;
+    public int Level3Cost = 800;
+    public Text level_0;
+    public Text level_1;
+    public Text level_2;
+    public Text level_3;
     void Start()
     {
+        miniMapCam = GameObject.Find("Minimap Camera").GetComponent<Camera>();
         publicRelations = FindObjectOfType<PublicRelations>();
-        Debug.Log(costs[0]);
-        Debug.Log(costs[1]);
-        Debug.Log(costs[2]);
+        costs = new Dictionary<SurveillanceLevel, int>
+        {
+            { SurveillanceLevel.Level_0, 0 },
+            { SurveillanceLevel.Level_1, Level1Cost },
+            { SurveillanceLevel.Level_2, Level2Cost },
+            { SurveillanceLevel.Level_3, Level3Cost }
+        };
 
     }
 
     void Update()
     {
+        CheckCost();
+    }
+
+    public void CheckCost()
+    {
         switch (current)
         {
             case SurveillanceLevel.Level_0:
+                if (costs[SurveillanceLevel.Level_1] > publicRelations._PRaccumulated)
+                    surveillanceButton.interactable = false;
+                else
+                    surveillanceButton.interactable = true;
                 break;
             case SurveillanceLevel.Level_1:
+                if (costs[SurveillanceLevel.Level_2] > publicRelations._PRaccumulated)
+                    surveillanceButton.interactable = false;
+                else
+                    surveillanceButton.interactable = true;
                 break;
             case SurveillanceLevel.Level_2:
+                if (costs[SurveillanceLevel.Level_3] > publicRelations._PRaccumulated)
+                    surveillanceButton.interactable = false;
+                else
+                    surveillanceButton.interactable = true;
                 break;
             case SurveillanceLevel.Level_3:
+                surveillanceButton.interactable = false;
                 break;
+        }
+    }
+
+    public void OnSurveillanceButtonClicked()
+    {
+        switch (current)
+        {
+            case SurveillanceLevel.Level_0:
+                current = SurveillanceLevel.Level_1;
+                level_0.enabled = false;
+                level_1.enabled = true;
+                miniMapCam.cullingMask |= 1 << LayerMask.NameToLayer("Reporter");
+                break;
+            case SurveillanceLevel.Level_1:
+                current = SurveillanceLevel.Level_2;
+                level_1.enabled = false;
+                level_2.enabled = true;
+                miniMapCam.cullingMask |= 1 << LayerMask.NameToLayer("BuildingDmg");
+                break;
+            case SurveillanceLevel.Level_2:
+                current = SurveillanceLevel.Level_3;
+                level_2.enabled = false;
+                level_3.enabled = true;
+                miniMapCam.cullingMask |= 1 << LayerMask.NameToLayer("Rioters");
+                break;
+
         }
     }
 }
