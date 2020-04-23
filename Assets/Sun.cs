a08596d4d44d77b9df2d257bc7d27101122edff7
Kerry Gougeon <kerrygougeon@gmail.com>
2020-04-22 21:52:32 -0400

    emission lights on building prefabs and bloom setting updated

---
+++ Assets/Sun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    public Light light;
+    public float startingIntensity;
+    public float minimumIntensity;
+    [Range(1,10)]
+    public float rate;
+    private float down;
+    // Start is called before the first frame update
+    void Start() { light.intensity = startingIntensity; }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        down += rate / 1000000;
+        var newIntensity = light.intensity - down;
+        if ( newIntensity < minimumIntensity ) newIntensity = minimumIntensity;
+        light.intensity = newIntensity;
+    }
+}
