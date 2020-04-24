e08f12fcfaf14b030a717fb4dd4e12428bc9270c
Jason Brennan <brenjaso@gmail.com>
2020-04-24 17:00:27 -0400

    Created simple firing squad style FX

---
+++ Assets/Scripts/TestFiringSquad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFiringSquad : MonoBehaviour
+{
+    public FiringSquad fs;
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            fs.Fire(target.position);
+    }
+}
